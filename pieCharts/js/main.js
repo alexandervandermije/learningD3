051459cfadfc2c58c13d2aa2def5661d686e6841
Alexander van der Mije <alexandervandermije@gmail.com>
2017-03-21 13:32:16 +0100

    pieChart & DonutCharts with text

--- pieCharts/js/main.js
+++ pieCharts/js/main.js
@@ -1,3 +1,4 @@
+
 var dataset = [
   { label: 'Abulia', count: 10 },
   { label: 'Betelgeuse', count: 20 },
@@ -11,26 +12,78 @@ var radius = Math.min(width, height) / 2;
 
 var color = d3.scaleOrdinal(d3.schemeCategory20b);
 
-var svg = d3.select('#chart')
-  	.append('svg')
-  	.attr('width', width)
+/* PIE Chart */
+
+var svgPie = d3.select('#pieChart') // select DOM element 
+  	.append('svg')// Set up SVG to draw on
+  	.attr('width', width) // Add a width and height to SVG
   	.attr('height', height)
-  	.append('g')
-  	.attr('transform', 'translate(' + (width / 2) +  ',' + (height / 2) + ')');
+  	.append('g')// Add G element
+  	.attr('transform', 'translate(' + (width / 2) +  ',' + (height / 2) + ')'); // transform said element to the middle of the SVG
 
-var arc = d3.arc()
-  	.innerRadius(0)
-  	.outerRadius(radius);
+var arc = d3.arc() //  Arc Function for dealing with the arc of the pie chart
+  	.innerRadius(0)// setting inner radius to 0 for piechart
+  	.outerRadius(radius); // setting outer radius to certain height
 
-var pie = d3.pie()
+var pie = d3.pie()// D3 Pie() function to turn normal data into pieChart
   	.value(function(d) { return d.count; })
   	.sort(null);
 
-var path = svg.selectAll('path')
-	.data(pie(dataset))
-	.enter()
-	.append('path')
-	.attr('d', arc)
-	.attr('fill', function(d, i) {
+var path = svgPie.selectAll('path')// select (for now non-existing) paths
+	.data(pie(dataset)) // pass the data from the dataset through the D3 Pie() function
+	.enter()// for every data entry,
+	.append('path')// add a path
+	.attr('d', arc)// with data passed through the arc function
+	.attr('fill', function(d, i) {// color the path based on the data.label passed through the color function
 		return color(d.data.label);
 	});
+
+/* Donut Chart */
+
+var donutWidth = 75;
+var legendRectSize = 18;
+var legendSpacing = 4;
+
+var svgDonut = d3.select('#donutChart')
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height)
+    .append('g')
+    .attr('transform', 'translate(' + (width / 2) +  ',' + (height / 2) + ')');
+
+var donutArc = d3.arc()
+  .innerRadius(radius - donutWidth)  // Instead of zero, for a donut chart add a width and subtract from the radius
+  .outerRadius(radius);
+
+var donutPath = svgDonut.selectAll('path')
+    .data(pie(dataset))
+    .enter()
+    .append('path')
+    .attr('d', donutArc)
+    .attr('fill', function(d, i) {
+        return color(d.data.label);
+    });
+
+var legend = svgDonut.selectAll('.legend') // select non-existing labels
+    .data(color.domain()) // because we passed the paths through the color function before, we now have a domain consisting of the colors & corresponding labels
+    .enter()
+    .append('g')
+    .attr('class', 'legend') // add the legend class
+    .attr('transform', function(d, i) { //  setting the location of the element
+        var height = legendRectSize + legendSpacing;
+        var offset =  height * color.domain().length / 2;
+        var horz = -2 * legendRectSize;
+        var vert = i * height - offset;
+        return 'translate(' + horz + ',' + vert + ')';
+    });
+
+legend.append('rect') // add a rectangle to the created legend
+    .attr('width', legendRectSize) // with a width, height and color
+    .attr('height', legendRectSize)
+    .style('fill', color)
+    .style('stroke', color);
+
+legend.append('text') // add text to the legend
+    .attr('x', legendRectSize + legendSpacing)// space the x and y of the text
+    .attr('y', legendRectSize - legendSpacing)
+    .text(function(d) { return d; });// select corresponding text from the data in the legend element
