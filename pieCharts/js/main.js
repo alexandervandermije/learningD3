836f15a2b574a32f2e8965f531ac75a22906bcec
Alexander van der Mije <alexandervandermije@gmail.com>
2017-03-22 16:24:52 +0100

    add interaction to donutChart

--- pieCharts/js/main.js
+++ pieCharts/js/main.js
@@ -1,10 +1,10 @@
+var app = angular.module('D3App', []);
+app.controller('myCtrl', function($scope) {
 
-var dataset = [
-  { label: 'Abulia', count: 10 },
-  { label: 'Betelgeuse', count: 20 },
-  { label: 'Cantaloupe', count: 30 },
-  { label: 'Dijkstra', count: 40 }
-];
+$scope.input1 = 10;
+$scope.input2 = 50;
+$scope.input3 = 10;
+$scope.input4 = 30;
 
 var width = 360;
 var height = 360;
@@ -12,31 +12,19 @@ var radius = Math.min(width, height) / 2;
 
 var color = d3.scaleOrdinal(d3.schemeCategory20b);
 
-/* PIE Chart */
+var tooltip = d3.select('#donutChart')           
+  .append('div')                            
+  .attr('class', 'tooltip');                
 
-var svgPie = d3.select('#pieChart') // select DOM element 
-  	.append('svg')// Set up SVG to draw on
-  	.attr('width', width) // Add a width and height to SVG
-  	.attr('height', height)
-  	.append('g')// Add G element
-  	.attr('transform', 'translate(' + (width / 2) +  ',' + (height / 2) + ')'); // transform said element to the middle of the SVG
+tooltip.append('div')                       
+  .attr('class', 'label');                  
 
-var arc = d3.arc() //  Arc Function for dealing with the arc of the pie chart
-  	.innerRadius(0)// setting inner radius to 0 for piechart
-  	.outerRadius(radius); // setting outer radius to certain height
+tooltip.append('div')                        
+  .attr('class', 'count');                   
 
-var pie = d3.pie()// D3 Pie() function to turn normal data into pieChart
-  	.value(function(d) { return d.count; })
-  	.sort(null);
+tooltip.append('div')                        
+  .attr('class', 'percent');                 
 
-var path = svgPie.selectAll('path')// select (for now non-existing) paths
-	.data(pie(dataset)) // pass the data from the dataset through the D3 Pie() function
-	.enter()// for every data entry,
-	.append('path')// add a path
-	.attr('d', arc)// with data passed through the arc function
-	.attr('fill', function(d, i) {// color the path based on the data.label passed through the color function
-		return color(d.data.label);
-	});
 
 /* Donut Chart */
 
@@ -44,46 +32,115 @@ var donutWidth = 75;
 var legendRectSize = 18;
 var legendSpacing = 4;
 
-var svgDonut = d3.select('#donutChart')
+
+var dataset = [
+  { label: 'Abulia', count: $scope.input1 },
+  { label: 'Betelgeuse', count: $scope.input2 },
+  { label: 'Cantaloupe', count: $scope.input3 },
+  { label: 'Dijkstra', count: $scope.input4 }
+];
+
+$scope.createDonutChart = function()
+{
+    $("#donutChart").html("");
+    var dataset = [
+        { label: 'Abulia', count: $scope.input1 },
+        { label: 'Betelgeuse', count: $scope.input2 },
+        { label: 'Cantaloupe', count: $scope.input3 },
+        { label: 'Dijkstra', count: $scope.input4 }
+    ];
+    console.log($scope.input1);
+    var svgDonut = d3.select('#donutChart')
     .append('svg')
     .attr('width', width)
     .attr('height', height)
     .append('g')
     .attr('transform', 'translate(' + (width / 2) +  ',' + (height / 2) + ')');
 
-var donutArc = d3.arc()
-  .innerRadius(radius - donutWidth)  // Instead of zero, for a donut chart add a width and subtract from the radius
-  .outerRadius(radius);
+    var donutArc = d3.arc()
+        .innerRadius(radius - donutWidth)  // Instead of zero, for a donut chart add a width and subtract from the radius
+        .outerRadius(radius);
 
-var donutPath = svgDonut.selectAll('path')
-    .data(pie(dataset))
-    .enter()
-    .append('path')
-    .attr('d', donutArc)
-    .attr('fill', function(d, i) {
+    var donutPath = svgDonut.selectAll('path')
+        .data(pie(dataset))
+        .enter()
+        .append('path')
+        .attr('d', donutArc)
+        .attr('fill', function(d, i) {
+            return color(d.data.label);
+        });
+
+    donutPath.on('mouseover', function(d) {
+        var total = d3.sum(dataset.map(function(d) {
+            return d.count;
+        }));
+
+        var percent = Math.round(1000 * d.data.count / total) / 10;
+        tooltip.select('.label').html(d.data.label);
+        tooltip.select('.count').html(d.data.count);
+        tooltip.select('.percent').html(percent + '%');
+        tooltip.style('display', 'block');
+    });
+    donutPath.on('mouseout', function() {
+        tooltip.style('display', 'none');
+    });
+
+    var legend = svgDonut.selectAll('.legend') // select non-existing labels
+        .data(color.domain()) // because we passed the paths through the color function before, we now have a domain consisting of the colors & corresponding labels
+        .enter()
+        .append('g')
+        .attr('class', 'legend') // add the legend class
+        .attr('transform', function(d, i) 
+        { //  setting the location of the element
+            var height = legendRectSize + legendSpacing;
+            var offset =  height * color.domain().length / 2;
+            var horz = -2 * legendRectSize;
+            var vert = i * height - offset;
+            return 'translate(' + horz + ',' + vert + ')';
+        });
+
+    legend.append('rect') // add a rectangle to the created legend
+        .attr('width', legendRectSize) // with a width, height and color
+        .attr('height', legendRectSize)
+        .style('fill', color)
+        .style('stroke', color);
+
+    legend.append('text') // add text to the legend
+        .attr('x', legendRectSize + legendSpacing)// space the x and y of the text
+        .attr('y', legendRectSize - legendSpacing)
+        .text(function(d) { return d; });// select corresponding text from the data in the legend element
+
+}
+
+/* PIE Chart */
+
+var svgPie = d3.select('#pieChart') // select DOM element 
+    .append('svg')// Set up SVG to draw on
+    .attr('width', width) // Add a width and height to SVG
+    .attr('height', height)
+    .append('g')// Add G element
+    .attr('transform', 'translate(' + (width / 2) +  ',' + (height / 2) + ')'); // transform said element to the middle of the SVG
+
+var arc = d3.arc() //  Arc Function for dealing with the arc of the pie chart
+    .innerRadius(0)// setting inner radius to 0 for piechart
+    .outerRadius(radius); // setting outer radius to certain height
+
+var pie = d3.pie()// D3 Pie() function to turn normal data into pieChart
+    .value(function(d) { return d.count; })
+    .sort(null);
+
+var path = svgPie.selectAll('path')// select (for now non-existing) paths
+    .data(pie(dataset)) // pass the data from the dataset through the D3 Pie() function
+    .enter()// for every data entry,
+    .append('path')// add a path
+    .attr('d', arc)// with data passed through the arc function
+    .attr('fill', function(d, i) {// color the path based on the data.label passed through the color function
         return color(d.data.label);
     });
 
-var legend = svgDonut.selectAll('.legend') // select non-existing labels
-    .data(color.domain()) // because we passed the paths through the color function before, we now have a domain consisting of the colors & corresponding labels
-    .enter()
-    .append('g')
-    .attr('class', 'legend') // add the legend class
-    .attr('transform', function(d, i) { //  setting the location of the element
-        var height = legendRectSize + legendSpacing;
-        var offset =  height * color.domain().length / 2;
-        var horz = -2 * legendRectSize;
-        var vert = i * height - offset;
-        return 'translate(' + horz + ',' + vert + ')';
-    });
 
-legend.append('rect') // add a rectangle to the created legend
-    .attr('width', legendRectSize) // with a width, height and color
-    .attr('height', legendRectSize)
-    .style('fill', color)
-    .style('stroke', color);
 
-legend.append('text') // add text to the legend
-    .attr('x', legendRectSize + legendSpacing)// space the x and y of the text
-    .attr('y', legendRectSize - legendSpacing)
-    .text(function(d) { return d; });// select corresponding text from the data in the legend element
+
+
+});
+
